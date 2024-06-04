dbd29328aeed69a60109ec69805996f4d5c8634f
Juan Pablo Saladino <juanpablosaladino@gmail.com>
2024-06-04 10:40:12 -0300

    Se agrega RecetaResource

---
+++ src/main/java/org/umsa/resource/RecetaResource.java
@@ -0,0 +1,33 @@
+package org.umsa.resource;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.umsa.bo.RecetaBO;
+
+@Path("/recetas")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RecetaResource {
+    @Inject
+    RecetaBO recetaBO;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getRecetas() {
+        try {
+            return Response
+                    .ok(recetaBO.getRecetas())
+                    .build();
+        } catch (Exception e) {
+            return Response
+                    .status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity("Error al obtener recetas")
+                    .build();
+        }
+    }
+}
